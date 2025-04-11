280c8bb5fce8f47ff0e4e4efe27fed6c094c3e22
ScrappyNozicka <vojtech2012@gmail.com>
2025-04-11 21:26:59 +0100

    battle class draft

---
+++ test/test_battle.py
@@ -0,0 +1,17 @@
+# class Battle:
+#     def __init__(self):
+#         self.pokemon_1 = None
+#         self.pokemon_2 = None
+#         self.pokemon_1_turn = 0
+#         self.pokemon_2_turn = 1
+
+#     def take_turn(self):
+#         pokemon.take_damage
+
+#     def get_winner(self):
+#         while pokemon_1.hp > 0 and pokemon_2.hp > 0:
+#             return None
+#         if pokemon_1.hp <= 0 :
+#             return pokemon_2
+#         if pokemon_2.hp <= 0:
+#             return pokemon_1
