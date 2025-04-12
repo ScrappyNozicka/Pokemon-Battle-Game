cda32e2715fa6950f639e257b7241dfeac649680
ScrappyNozicka <vojtech2012@gmail.com>
2025-04-12 19:47:40 +0100

    main script notes

---
+++ src/pokemon_game.py
@@ -0,0 +1,12 @@
+def pokemon_game_script():
+    pass
+
+
+#into welcome screen
+#input option select game
+#input trainer names
+#input pokemon
+#start game
+#result window
+#input play again or exit
+#exit window
---
+++ test/test_pokemon_game.py
@@ -0,0 +1,2 @@
+def test_pokemon_game_script():
+    pass
