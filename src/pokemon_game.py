cc559c36e3a3d61de66d5283e4f9cc27540a3b40
ScrappyNozicka <vojtech2012@gmail.com>
2025-04-18 10:46:26 +0100

    main script working update to accomodate for betetr pokemon selection and to enforce 1 minimum only

--- src/pokemon_game.py
+++ src/pokemon_game.py
@@ -3,6 +3,7 @@ from utils_funcs import (
     pokemon_data_reader,
     pokemon_table_display,
     get_pokemon_data,
+    catch_random_pokemon
 )
 from battle import Battle
 from trainer import Trainer
@@ -47,26 +48,111 @@ def main_script():
         )
         trainer_01 = Trainer(trainer_01_name)
         print(trainer_01)
+        if trainer_01.space_on_belt == 6:
+            pokemon_selection_01 = input_manager("Challenger, do you want to [s]elect your first pokemon or choose one at [r]andom?\n")
+            while pokemon_selection_01.lower() not in "sr":
+                pokemon_selection_01 = input_manager(
+                    "Invalid input. Please specify: "
+                    "[s]elect your pokemon or choose [r]andom pokemon.\n"
+                )
+            if pokemon_selection_01.lower() == "s":
+                while True:
+                    pokemon_id = input_manager("What's the pokemon id? (or type 'q' to cancel)\n\n")
+                    if pokemon_id.lower() == 'q':
+                        print("Cancelled Pokémon selection.")
+                        break
+                    pokemon = get_pokemon_data(pokemon_id)
+                    if pokemon:
+                        trainer_01.throw_pokeball(pokemon)
+                        break
+                    else:
+                        print("Pokemon not found, please try again.")
+            elif pokemon_selection_01.lower() == "r":
+                random_pokemon = catch_random_pokemon()
+                trainer_01.throw_pokeball(random_pokemon)
+
+
         while trainer_01.belt_space():
-            pokemon_id = input_manager("What's the challenger's pokemon id?\n")
-            pokemon = get_pokemon_data(pokemon_id)
-            if not pokemon:
-                print("Pokemon not found, please try again.")
-            else:
-                trainer_01.throw_pokeball(pokemon)
+            further_selection_01 = input_manager("Challenger, do you want to catch further pokemon? [y/n]\n")
+            while further_selection_01.lower() not in "yn":
+                further_selection_01 = input_manager(
+                    "Invalid input. Please specify: "
+                    "[y] to catch further pokemon or [n] to start the battle.\n"
+                )
+            if further_selection_01.lower() == "y": 
+                pokemon_selection_1 = input_manager("Challenger, do you want to [s]elect your pokemon or choose one at [r]andom?\n")
+                if pokemon_selection_1.lower() == "s":
+                    pokemon_id = input_manager("What's the pokemon id?\n")
+                    pokemon = get_pokemon_data(pokemon_id)
+                    if not pokemon:
+                        print("Pokemon not found, please try again.")
+                    else:
+                        trainer_01.throw_pokeball(pokemon)
+                if pokemon_selection_1.lower() == "r":
+                    random_pokemon = catch_random_pokemon()
+                    trainer_01.throw_pokeball(random_pokemon)
+            if further_selection_01.lower() == "n":
+                print("thank you for your choice.\n")
+                break
+                        
 
         trainer_02_name = input_manager(
             "What's the defender's trainer name?\n"
         )
         trainer_02 = Trainer(trainer_02_name)
         print(trainer_02)
+        if trainer_02.space_on_belt == 6:
+            pokemon_selection = input_manager("Challenger, do you want to [s]elect your first pokemon or choose one at [r]andom?\n")
+            while response.lower() not in "sr":
+                response = input_manager(
+                    "Invalid input. Please specify: "
+                    "[s]elect your pokemon or choose [r]andom pokemon.\n"
+                )
+            if response == "s":
+                pokemon_id = input_manager("What's the pokemon id?\n")
+                pokemon = get_pokemon_data(pokemon_id)
+                if not pokemon:
+                    print("Pokemon not found, please try again.")
+                else:
+                    trainer_02.throw_pokeball(pokemon)
+            if response == "r":
+                random_pokemon = catch_random_pokemon()
+                trainer_02.throw_pokeball(random_pokemon)
+
         while trainer_02.belt_space():
-            pokemon_id = input_manager("What's the defender's pokemon id?\n")
-            pokemon = get_pokemon_data(pokemon_id)
-            if not pokemon:
-                print("Pokemon not found, please try again.")
-            else:
-                trainer_02.throw_pokeball(pokemon)
+            response = input_manager("Challenger, do you want to catch further pokemon? [y/n]\n")
+            while response.lower() not in "yn":
+                response = input_manager(
+                    "Invalid input. Please specify: "
+                    "[y] to catch further pokemon or [n] to start the battle.\n"
+                )
+            if response == "y": 
+                response = input_manager("Challenger, do you want to [s]elect your pokemon or choose one at [r]andom?\n")
+                if response == "s":
+                    pokemon_id = input_manager("What's the pokemon id?\n")
+                    pokemon = get_pokemon_data(pokemon_id)
+                    if not pokemon:
+                        print("Pokemon not found, please try again.")
+                    else:
+                        trainer_02.throw_pokeball(pokemon)
+                if response == "r":
+                    random_pokemon = catch_random_pokemon()
+                    trainer_02.throw_pokeball(random_pokemon)
+            if response == "n":
+                print("thank you for your choice.\n")
+                break
+
+
+
+
+
+
+
+
+
+
+
+
 
         pokemon_01_name = input_manager(
             f"{trainer_01.name} which pokemon do you send out first?\n"
@@ -76,18 +162,19 @@ def main_script():
         )
 
         pokemon_01 = trainer_01.get_pokemon_by_name(pokemon_01_name)
-        if not pokemon_01:
-            print(f"{pokemon_01} not found! Please try again.")
+        while not pokemon_01:
             pokemon_01_name = input_manager(
+                f"{pokemon_01} not found! Please try again."
                 f"{trainer_01.name} which pokemon do you send out first?\n"
-            )
+        )
 
         pokemon_02 = trainer_02.get_pokemon_by_name(pokemon_02_name)
-        if not pokemon_02:
-            print(f"{pokemon_02} not found! Please try again.")
+        while not pokemon_02:
             pokemon_02_name = input_manager(
+                f"{pokemon_02} not found! Please try again."
                 f"{trainer_02.name} which pokemon do you send out first?\n"
             )
+
 
         print(f"\n{trainer_01.name} has chosen:\n{pokemon_01}")
         print(f"\n{trainer_02.name} has chosen:\n{pokemon_02}\n")
