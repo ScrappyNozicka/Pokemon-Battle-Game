87233d521869f70f7ce494043ca770392e5d030e
ScrappyNozicka <vojtech2012@gmail.com>
2025-04-27 12:42:15 +0100

    complete testing for main script for exit, listing and incorrect input

--- test/test_pokemon_game.py
+++ test/test_pokemon_game.py
@@ -1,6 +1,69 @@
-# from data.pokemon_game import pokemon_game
+from src.pokemon_game import pokemon_game, main_script
+from unittest.mock import patch
 
 
-def test_pokemon_game_script():
-    # pokemon_game()
-    pass
+@patch("builtins.input", side_effect=["e"])
+@patch("builtins.print")
+def test_pokemon_game_return_welcome_message(mock_print, mock_input):
+    pokemon_game()
+
+    mock_print.assert_any_call(
+        """
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                              Welcome to
+                        Pokemon Battle Simulator
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    """
+    )
+
+
+@patch("builtins.input", side_effect=["999", "e"])
+def test_pokemon_game_main_script_invalid_input(mock_input):
+    main_script()
+
+    mock_input.assert_any_call(
+        "Please specify: "
+        "[l]ist available pokemon, [p]lay a match or [e]xit the game.\n"
+    )
+    mock_input.assert_any_call(
+        "Invalid input. Please specify: "
+        "[l]ist available pokemon, [p]lay a match "
+        "or [e]xit the game.\n"
+    )
+
+
+@patch("builtins.input", side_effect=["e"])
+@patch("builtins.print")
+def test_pokemon_game_main_script_exit_the_game(mock_print, mock_input):
+    main_script()
+
+    mock_print.assert_any_call(
+        "Thank you for playing. Come battle again soon."
+    )
+
+
+@patch("src.pokemon_game.pokemon_table_display")
+@patch("src.pokemon_game.pokemon_data_reader")
+@patch("builtins.input", side_effect=["l", "e"])
+def test_pokemon_game_main_script_listing_pokemon(
+    mock_input, mock_reader, mock_display
+):
+
+    mock_reader.return_value = ["Infernus", "Typicus"]
+
+    main_script()
+
+    mock_reader.assert_called_once()
+    mock_display.assert_called_once_with(["Infernus", "Typicus"])
+
+
+# def test_pokemon_game_main_script_challenger_wins_match(
+# mock_print, mock_input):
+# def test_pokemon_game_main_script_defender_wins_match(
+# mock_print, mock_input):
+
+# def test_pokemon_game_main_script_challenger_selected_no_pokemon(
+# mock_print, mock_input):
+# def test_pokemon_game_main_script_defender_selected_no_pokemon(
+# mock_print, mock_input):
